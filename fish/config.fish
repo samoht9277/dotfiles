9b8ee634c3eee61085aa6e93f8e652874e3538ac
samoht <53660242+samoht9277@users.noreply.github.com>
2022-03-29 11:10:36 -0300

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# THEME PURE #
+set fish_function_path /home/tomi/.config/fish/functions/theme-pure/functions/ $fish_function_path
+source /home/tomi/.config/fish/functions/theme-pure/conf.d/pure.fish
